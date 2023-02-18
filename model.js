b8e11be2f02174ccbc21bc1fd3d8a0e34d9a1086
Yahya Ameen <yahyaameen33@gmail.com>
2023-02-18 21:06:44 +0200

    adding getPostIndexById function to model.js and update other function in this file

--- model.js
+++ model.js
@@ -2,6 +2,17 @@ const Tweeter = function() {
     let _postIdCounter = 0
     let _commentIdCounter = 0
     const _posts = posts
+    const INVALID_INDEX = -1
+
+    const getPostIndexById = function(postId) {
+        for (const i in _posts) {
+            const post = _posts[i]
+            if (post.id === postId) {
+                return i
+            }
+        }
+        return INVALID_INDEX
+    }
 
     const getPosts = () => _posts
 
@@ -11,34 +22,32 @@ const Tweeter = function() {
         _posts.push(post)
     }
 
-    const removePost = function(postID) {
-        for (const i in _posts) {
-            if (_posts[i].id === postID) {
-                _posts.splice(i, 1)
-            }
+    const removePost = function(postId) {
+        const postIndex = getPostIndexById(postId)
+        if (postIndex !== INVALID_INDEX) {
+            _posts.splice(postIndex, 1)
         }
     }
 
     const addComment = function(commentText, postId) {
         _commentIdCounter++
         const comment = { id: "c" + _commentIdCounter, text: commentText }
-        for (const post of _posts) {
-            if (post.id === postId) {
-                post.comments.push(comment)
-            }
-        }
+        const postIndex = getPostIndexById(postId)
+        const post = _posts[postIndex]
+        post.comments.push(comment)
     }
 
-    const removeComment = function(postID, commentID) {
-        for (const post of _posts) {
-            if (post.id === postID) {
-                for (const i in post.comments) {
-                    if (post.comments[i].id === commentID) {
-                        post.comments.splice(i, 1)
-                    }
-                }
+    const removeComment = function(postId, commentId) {
+        const postIndex = getPostIndexById(postId)
+        const post = _posts[postIndex]
+        const postComments = post.comments
+        for (const i in postComments) {
+            const postComment = postComments[i]
+            if (postComment.id === commentId) {
+                postComments.splice(i, 1)
             }
         }
+
     }
 
     return {
